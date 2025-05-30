63d06524aa9f7b2e5c0ef56455199fe77456ae94
samandar_akbarov <samandar_akbarov@epam.com>
2025-05-30 08:08:10 +0500

    Avoid null pointer exception

--- grpc-server/src/main/java/live/akbarov/grpcserver/service/MessageService.java
+++ grpc-server/src/main/java/live/akbarov/grpcserver/service/MessageService.java
@@ -7,6 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.grpc.server.service.GrpcService;
 
+import java.util.Optional;
+
 @GrpcService
 public class MessageService extends MessageServiceGrpc.MessageServiceImplBase {
     private static final Logger logger = LoggerFactory.getLogger(MessageService.class);
@@ -14,11 +16,13 @@ public class MessageService extends MessageServiceGrpc.MessageServiceImplBase {
     @Override
     public void doAction(Message request, StreamObserver<Message> observer) {
         logger.info("Message received {}", request);
-        String responseMessage = "Pong";
-        if (!"Ping".equals(request.getMessage())) {
-            logger.warn("Received unexpected message: {}", request.getMessage());
-        }
-        observer.onNext(Message.newBuilder().setMessage(responseMessage).build());
+
+        Optional.of(request)
+                .map(Message::getMessage)
+                .filter("Ping"::equals)
+                .ifPresentOrElse(
+                        message -> observer.onNext(Message.newBuilder().setMessage("Pong").build()),
+                        () -> logger.warn("Received unexpected message: {}", request));
         observer.onCompleted();
     }
 }
